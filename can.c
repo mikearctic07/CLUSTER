553cc0ee58d8ee232680091cd8fd145c53d0d152
rbn <boorred@gmail.com>
2018-12-06 17:21:36 -0600

    Addition of useful variables in can.c for data input acquisition by can

--- can.c
+++ can.c
@@ -1,1 +1,8 @@
 #include "can.h"
+
+uint32_t  RxCODE;              /* Received message buffer code */
+uint32_t  RxID;                /* Received message ID */
+uint32_t  RxLENGTH;            /* Recieved message number of data bytes */
+uint32_t  RxTIMESTAMP;         /* Received message time */
+
+
